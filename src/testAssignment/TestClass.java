71081a2e93ac9b766f862d3f030561cb701e3a9a
SurabhiSadanand <surabhisadanand92@gmail.com>
2022-05-22 11:41:41 -0400

    new test Added

---
+++ src/testAssignment/TestClass.java
@@ -0,0 +1,10 @@
+package testAssignment;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is testing class for assignment");
+	}
+
+}
