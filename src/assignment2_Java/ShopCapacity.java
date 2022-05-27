4fa64110eb164b26b6a1b13f3eb8c63711e7c275
SurabhiSadanand <surabhisadanand92@gmail.com>
2022-05-27 17:31:37 -0400

    Changes to Question2 After review

--- src/assignment2_Java/ShopCapacity.java
+++ src/assignment2_Java/ShopCapacity.java
@@ -6,18 +6,28 @@ public class ShopCapacity {
 
 		int maxShopCapacity = 50;
 		int currentCount = 48;
-		if (currentCount > 50) {
+		int entryCount=2;
+		int exitCount=1;
+		System.out.println("Current count of people in the shop: "+ (currentCount));
+		if (currentCount >= 50) {
 			System.out.println(
 					"Maximum capacity of 50 people reached, \n Cannot serve more people due to Covid restrictions !!!");
 		} else {
-			while (currentCount <= maxShopCapacity) {
-				if (currentCount == 50) {
-					System.out.println("***Maximum capacity of 50 reached***");
-					break;
+		while (currentCount+entryCount-exitCount< maxShopCapacity) {
+				
+				if((maxShopCapacity - currentCount) >=entryCount&& (currentCount + entryCount -exitCount) < maxShopCapacity) {
+					System.out.println("Adding "+ entryCount+ " people in the shop");
+					currentCount= currentCount + entryCount;
+					System.out.println("Number of People in Shop: " + currentCount);
 				}
-				System.out.println("Number of People in Shop: " + currentCount);
-				currentCount++;
-				System.out.println("Adding next customer...");
+				if(currentCount+entryCount>maxShopCapacity)
+				{
+					System.out.println("Cannot add entrycount of "+entryCount);
+				}
+				if(currentCount>=50) {
+					System.out.println(
+							"Maximum capacity of 50 people reached, \n Cannot serve more people due to Covid restrictions !!!");
+				}
 			}
 			System.out.println("Total Number of People in Shop: " + currentCount);
 		}
